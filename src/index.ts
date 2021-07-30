742dd443b4c16aa5998c4b52e10295230ee5b0d3
Vincent Racelle <vracelle@positivethinking.lu>
2021-07-30 10:31:58 +0200

    initial config for docker and typescript

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, {Application, Request, Response, NextFunction} from 'express';
+
+const port = Number(process.env.PORT || 4000);
+
+const logRequest = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req);
+    next();
+}
+
+const app: Application = express();
+app.use(logRequest);
+
+app.get( "/", (req, res) => {
+    res.send( "Hello From Express With TypeScript" );
+});
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
+});
